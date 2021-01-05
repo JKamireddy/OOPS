13109c3c04b202e9fe3482b6128c5e25692ee8b9
Jahnavi Kamireddy <44324161+JKamireddy@users.noreply.github.com>
2021-01-05 09:13:30 -0500

    Update assign1.cc

--- Project 7/assign1.cc
+++ Project 7/assign1.cc
@@ -1,18 +1,5 @@
 
 
-/******************************************************************************
-  *  Name: SAHITHI KAMIREDDY                                                    *
-  *  Z:ID: Z1831177                                                           *
-  *  Class: CSCI 689                                                     *
-  *  Date: 02-01-2018.                                                        *
-  *  Description: Printing the Heat Index table using Air Temperatures                                                                         *
-  *               and Relative Humidity.
-    Instructor: Dr.Duffin
-	   TA: Nithin Devang                                                         *
-  *                                                                           *
-  *                                                                           *
-******************************************************************************/
-
 #include<iostream>
 #include<iomanip>
 #include<cmath>
